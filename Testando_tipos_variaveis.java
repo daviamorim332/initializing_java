da3677d6e6fa1853d608e3bae862c98171be21be
daviamorim332 <83930938+daviamorim332@users.noreply.github.com>
2021-05-11 14:55:32 -0300

    Add files via upload

---
+++ Testando_tipos_variaveis.java
@@ -0,0 +1,21 @@
+
+
+public class Testando_tipos_variaveis {
+
+	public static void main(String[] args) {
+		
+		String nome1 ="casa";
+		boolean eclipse_ide= true;
+		int number1 = 22;
+		double note = 33.87;
+
+		
+		
+		System.out.println(nome1);
+		System.out.println(eclipse_ide);
+		System.out.println(number1);
+		System.out.println(note);
+
+	}
+
+}
